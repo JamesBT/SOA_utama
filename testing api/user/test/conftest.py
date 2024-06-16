8a6269f1db5d7e443abc51ff9056a506526519db
JamesBT <c14210026@john.petra.ac.id>
2024-06-16 20:06:39 +0700

    teesting api
    teesting api
    udah hampir bisa ke launch masih ada error aneh
    gateway import user
    user import dependencies error

--- testing api/user/test/conftest.py
+++
@@ -1,9 +0,0 @@
-# conftest.py
-import pytest
-from unittest.mock import create_autospec
-from dependencies import DatabaseWrapper
-
-@pytest.fixture(scope="session")
-def mock_db_connection():
-    # Create a mock database connection
-    return create_autospec(DatabaseWrapper)
--- testing api/user/user/service.py
+++ testing api/user/user/service.py
@@ -1,6 +1,6 @@
 from nameko.rpc import rpc
 
-import dependencies
+from user import dependencies
 
 class userService:
 
