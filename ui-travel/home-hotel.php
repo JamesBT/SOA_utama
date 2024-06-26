1d477927128687f27da1b79d9cb5bb5cb4014890
JamesBT <c14210026@john.petra.ac.id>
2024-06-27 00:18:42 +0700

    travel agent gabung
    search travel agent di delete aja,
    
    di halaman search itu ganti ke 3 kotak 1 ke louis, 1 ke nicho, 1 ke timothy

--- ui-travel/home-hotel.php
+++ ui-travel/home-hotel.php
@@ -61,7 +61,7 @@
 			<header class="absolute inset-x-0 top-0 z-50 text-white">
 				<nav class="flex items-center justify-between p-6 lg:px-8 clearfix" aria-label="Global">
 					<div class="flex lg:flex-1">
-						<a href="home.html" class="-m-1.5 p-1.5">
+						<a href="home.php" class="-m-1.5 p-1.5">
 						<span class="sr-only">Your Company</span>
 						<img class="h-8 w-auto" src="images/logo.png" alt="">
 						</a>
@@ -75,7 +75,7 @@
 						</button>
 					</div>
 					<div class="hidden lg:flex lg:gap-x-12">
-						<a href="home.html" class="text-lg font-semibold leading-6 text-white hover:animate-bounce">Home</a>
+						<a href="home.php" class="text-lg font-semibold leading-6 text-white hover:animate-bounce">Home</a>
 						<a href="#about-us" class="text-lg font-semibold leading-6 text-white transition duration-500 ease-in-out hover:animate-bounce">About Us</a>
                         <a href="#location" class="text-lg font-semibold leading-6 text-white transition duration-500 ease-in-out hover:animate-bounce">Location</a>
 						<a href="#rooms" class="text-lg font-semibold leading-6 text-white transition duration-500 ease-in-out hover:animate-bounce">Rooms</a>						
@@ -90,7 +90,7 @@
 					<div class="fixed inset-0 z-50 bg-black bg-opacity-25" @click="isOpen = false"></div>
 					<div class="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10 transition transform duration-300" :class="{ 'translate-x-0': isOpen, 'translate-x-full': !isOpen }">
 						<div class="flex items-center justify-between">
-							<a href="home.html" class="-m-1.5 p-1.5">
+							<a href="home.php" class="-m-1.5 p-1.5">
 							<span class="sr-only">Your Company</span>
 							<img class="h-8 w-auto" src="images/logo.png" alt="">
 							</a>
@@ -104,7 +104,7 @@
 						<div class="mt-6 flow-root">
 							<div class="-my-6 divide-y divide-gray-500/10">
 								<div class="space-y-2 py-6">
-									<a href="home.html"  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</a>
+									<a href="home.php"  class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Home</a>
 									<a href="#about-us" @click="isOpen = false" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">About Us</a>
                                     <a href="#location" @click="isOpen = false" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Location</a>
 									<a href="#rooms" @click="isOpen = false" class="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">Rooms</a>
